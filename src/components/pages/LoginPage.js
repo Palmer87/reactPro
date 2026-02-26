116db6a30fcadff7ab74eab81edd361fd290a5ca
Djue Kouassi Celestin <palmer8744@gmail.com>
2026-02-26 15:04:56 +0000

    Oublier de add un element

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function () {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`bonjour ! ${inputValue}`);
+        setInputValue('');
+    
+    }
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    } 
   return (
     <div>
-        <h1>bienvenue</h1>
+        <h1>Benvenue chez nous</h1>
+        <br />
+        <h2>Connectez-vous</h2>
+        <form action='submit' onSubmit={handleSubmit}>
+            <input 
+            type='text' 
+            value={inputValue} 
+            onChange={handleChange}
+            placeholder='Entrez votre prenom...' required />
+           <button type="submit">Acceder à votre espace</button>
+        </form>
     </div>
   )
 }
